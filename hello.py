3b92705481625aef030b8e6ec2601622e70a0166
sun-ck <sun123scc@163.com>
2018-11-18 23:01:13 +0800

    问好程序2.0

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 #! python3
+#这个程序用来向用户问好并登记用户的名字
 
-print('你好，世界！')
-input()
+print('尊敬的用户，您好！')
+print('请输入您的姓名或昵称：') #询问用户姓名或昵称
+user_name = input()
+print('欢迎您加入本社区'+user_name)
+print('请输入您的年龄：')
+user_age = input()
+print('您快要'+str(int(user_age)+1)+'呢了')
